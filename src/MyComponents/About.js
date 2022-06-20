beac0f900e0e735b056054a2c099ae2d407c9bc2
Saurabh Phawade <saurvphawade09@gmail.com>
2022-06-20 22:52:57 +0530

    todo list

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <div>
+        This is an About Component
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt aperiam atque officiis id corrupti? Nostrum facilis sequi beatae deserunt debitis consectetur nobis non vitae, id obcaecati doloribus alias aliquam modi explicabo incidunt, possimus dolorum!</p>
+    </div>
+  )
+}
